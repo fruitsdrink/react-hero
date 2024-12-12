73767aae15556b097d08f534a58dc2732fa986e1
haightgao <haightgao@gmail.com>
2024-12-12 13:14:05 +0000

    shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
       <div className="bg-red-800 text-white">main</div>
+      <div>
+        <Button>main</Button>
+      </div>
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     open: true
   },
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
