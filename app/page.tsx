7aa5c497fa7b1dd6bc54c3b26fdc568c8293a203
fruitsdrink <fruitsdrink@126.com>
2025-11-09 10:56:06 +0800

    Add key metrics page and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Link href="/portfolio-planet">portfolio-planet</Link>
+      <Link href="/key-metrics">key-metrics</Link>
     </div>
   );
 }
